4cba2d258b5c14021183cd3183f8df8af5a1e8c5
PavelKushnar <44687563+PavelKushnar@users.noreply.github.com>
2019-07-01 10:56:07 +0300

    slot_machine 1.0

---
+++ slots.h
@@ -0,0 +1,32 @@
+#include <SFML\Graphics.hpp>
+const int HEIGHT_slots = 25;//размер ленты высота
+const int WIDTH_slots = 5;//размер ленты ширина
+
+
+sf::String TileSlots[HEIGHT_slots] = {
+	"fbcde",
+	"afcde",
+	"bbcde",
+	"abcde",
+	"abfde",
+	"abcde",
+	"abcfe",
+	"abcfe",
+	"afcde",
+	"accde",
+	"abcde",
+	"fbcde",
+	"abcde",
+	"abcde",
+	"abcde",
+	"fbcde",
+	"fbcfe",
+	"accde",
+	"abcde",
+	"fbcde",
+	"abcde",
+	"abcde",
+	"abcde",
+	"abcde",
+	"abcde",
+};
